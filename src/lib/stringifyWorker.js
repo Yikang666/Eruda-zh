7b829fe6487817309c31a5fcb6b34b24a71cd06c
surunzi <redhoodsu@tencent.com>
2020-01-07 15:57:17 +0800

    chore: small changes

--- src/lib/stringifyWorker.js
+++
@@ -1,7 +0,0 @@
-import stringifyAll from 'licia/stringifyAll'
-
-onmessage = function(e) {
-  const [id, obj, options] = e.data
-  const result = stringifyAll(obj, options)
-  postMessage([id, result])
-}
